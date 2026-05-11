130353f33a46374185a42aa1f467ed65dad3294e
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-05-11 14:52:13 +0200

    cleanup: sonar issues

--- src/main/java/no/nav/oebs/restapi/api/vieri/konteringsinfohbvieri/v1/KonteringsinfoHbServiceVieri.java
+++ src/main/java/no/nav/oebs/restapi/api/vieri/konteringsinfohbvieri/v1/KonteringsinfoHbServiceVieri.java
@@ -23,15 +23,15 @@ public class KonteringsinfoHbServiceVieri extends ObjektMaps {
 		super(objectMapper);
 		this.plsqlProcedureRepository = plsqlProcedureRepository;
 	}
-	public String finnKonteringsinfoHbVieritransaksjoner(Integer org_id, String segmentname, String segmentverdi, LocalDate lastupdatedate ) {
+	public String finnKonteringsinfoHbVieritransaksjoner(Integer orgid, String segmentname, String segmentverdi, LocalDate lastupdatedate ) {
 
-		PlsqlProcedureResult result = executePlsqlProcedure(buildRequest(org_id, segmentname, segmentverdi, lastupdatedate));
+		PlsqlProcedureResult result = executePlsqlProcedure(buildRequest(orgid, segmentname, segmentverdi, lastupdatedate));
 		return result.getData();
 
 	}
-	private KontRequest buildRequest(Integer org_id, String segmentname, String segmentverdi, LocalDate lastupdatedate) {
+	private KontRequest buildRequest(Integer orgid, String segmentname, String segmentverdi, LocalDate lastupdatedate) {
 		return KontRequest.builder()
-				.org_id(org_id) //
+				.org_id(orgid) //
 				.segmentname(segmentname) //
 				.segmentverdi(segmentverdi) //
 				.lastupdatedate(lastupdatedate) //
