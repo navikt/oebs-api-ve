72d69e39ccbae1deb15fd1221531f5e2c33d6327
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-05-21 10:17:59 +0200

    fix: possible security hotspot when handling regex

--- src/main/java/no/nav/oebs/restapi/config/common/logging/LoggingUtils.java
+++ src/main/java/no/nav/oebs/restapi/config/common/logging/LoggingUtils.java
@@ -1,27 +1,18 @@
 package no.nav.oebs.restapi.config.common.logging;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.util.regex.Pattern;
 
 public class LoggingUtils {
 
+	private static final Pattern FNR_PATTERN = Pattern.compile("(\\D+|^)(\\d{2})\\d{7}(\\d{2})(\\D+|$)");
+
 	private LoggingUtils() {
-
-	}
-
-	public static String formatExceptionAsString(Throwable exception) {
-		if (exception == null) {
-			return null;
-		}
-		var stringWriter = new StringWriter();
-		exception.printStackTrace(new PrintWriter(stringWriter));
-
-		return stringWriter.toString();
 	}
 
 	public static String maskIfFnr(String text) {
-		return text != null //
-				? text.replaceAll("([^0-9]+|^)([0-9]{2})[0-9]{7}([0-9]{2})([^0-9]+|$)", "$1$2" + "*******" + "$3$4") //
-				: "(null)";
+		if (text == null) {
+			return "(null)";
+		}
+		return FNR_PATTERN.matcher(text).replaceAll("$1$2" + "*******" + "$3$4");
 	}
 }
